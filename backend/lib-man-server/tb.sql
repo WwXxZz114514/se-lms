4ecb9008ea258bc0f06d5b64af9e471b3a7ba79a
Wxz17607604189 <2307019155@qq.com>
2023-12-28 13:44:41 +0800

    update sql

--- backend/lib-man-server/tb.sql
+++ backend/lib-man-server/tb.sql
@@ -1,27 +1,36 @@
+DROP TABLE IF EXISTS tb_user;
 CREATE TABLE tb_user (
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(32) NOT NULL UNIQUE,
     tel CHAR(11) NOT NULL UNIQUE,
     password VARCHAR(16) NOT NULL
 );
+
+DROP TABLE IF EXISTS tb_admin;
 CREATE TABLE tb_admin (
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(32) NOT NULL UNIQUE,
     tel CHAR(11) NOT NULL UNIQUE,
     password VARCHAR(16) NOT NULL
 );
+
+DROP TABLE IF EXISTS tb_seat;
 CREATE TABLE tb_seat (
     seat_id INT PRIMARY KEY AUTO_INCREMENT,
     seat_row INT NOT NULL,
     seat_col INT NOT NULL,
     area_id INT NOT NULL
 );
+
+DROP TABLE IF EXISTS tb_area;
 CREATE TABLE tb_area (
     area_id INT PRIMARY KEY AUTO_INCREMENT,
     area_name VARCHAR(32) NOT NULL,
     area_rows INT NOT NULL DEFAULT 2,
     area_cols INT NOT NULL DEFAULT 2
 );
+
+DROP TABLE IF EXISTS tb_order;
 CREATE TABLE tb_order (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
@@ -31,6 +40,7 @@ CREATE TABLE tb_order (
     order_status INT NOT NULL DEFAULT 0,
     UNIQUE (seat_id, appointment_time)
 );
+
 INSERT INTO tb_admin(username, tel, password) VALUES ('admin', '12345678901', '123456');
 INSERT INTO tb_user(username, tel, password) VALUES ('user', '12345678901', '123456');
 INSERT INTO tb_area(area_name, area_cols, area_rows) VALUES ('Lib1', 5, 5), ('Lib2', 2, 2);
